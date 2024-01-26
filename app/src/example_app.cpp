4537c39dfc0168442f65219ecf902f3d2c238607
Lukas Simon <lukas@flecs.tech>
2024-01-26 15:20:04 +0100

    Correctly unsubscribe only subscribed topics

--- app/src/example_app.cpp
+++ app/src/example_app.cpp
@@ -126,7 +126,8 @@ auto exec_example_loop() //
             std::this_thread::sleep_for(std::chrono::seconds(5));
         };
 
-        flunder_client.unsubscribe("**");
+        flunder_client.unsubscribe("/flecs/flunder/cpp/**");
+        flunder_client.unsubscribe("/flecs/flunder/external");
         flunder_client.disconnect();
 
         std::this_thread::sleep_for(std::chrono::seconds(5));
